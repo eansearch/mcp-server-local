8b27072bddd01a78799363f009aa028299ba6aa0
Jan Willamowius <jan@willamowius.de>
2025-09-18 16:59:57 +0200

    add isbn search

--- ean-search-mcp.py
+++ ean-search-mcp.py
@@ -1,29 +1,35 @@
 import os
+import re
 from eansearch import EANSearch
 from mcp.server.fastmcp import FastMCP
 
 # Initialize FastMCP server
-mcp = FastMCP("eansearch")
+app = FastMCP("eansearch")
 ean_search_api_token = os.environ['EAN_SEARCH_API_TOKEN']
 
 eansearch = EANSearch(ean_search_api_token)
 
-@mcp.tool()
-async def lookup_product(ean: str) -> str:
-    """Lookup the product for an EAN, GTIN or UPC barcode.
+
+@app.tool()
+async def lookup_product(barcode: str) -> str:
+    """Lookup the product for an EAN, GTIN, ISBN or UPC barcode.
 
     Args:
-        ean: barcode
+        barcode: the barcode (EAN, GTIN, ISBN or UPC)
     """
     if not ean_search_api_token:
         return "You need to set put your API token into the environment variable EAN_SEARCH_API_TOKEN. Get your API token from https://www.ean-search.org/ean-database-api.html"
-    ean = ean.strip()
-    data = eansearch.barcodeLookup(ean)
+    barcode = re.sub('[^0-9Xx]', '', barcode)
+    if len(barcode) == 10:
+        data = eansearch.isbnLookup(barcode)
+        barcode = f"ISBN {barcode}" # for output below
+    else:
+        data = eansearch.barcodeLookup(barcode)
     if not data:
         return "No product found."
-    return ean + " is product " + data
+    return barcode + " is product " + data
 
-@mcp.tool()
+@app.tool()
 async def find_products(keywords: str) -> str:
     """Find products matching all the keywords with their EAN barcode.
 
@@ -48,4 +54,4 @@ async def find_products(keywords: str) -> str:
 
 if __name__ == "__main__":
     # Initialize and run the server
-    mcp.run(transport='stdio')
+    app.run(transport='stdio')
